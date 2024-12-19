806b0d2bb14561945ea3920925547cc60f252668
kpeich <13398635+kpeich@users.noreply.github.com>
2024-12-18 17:36:58 -0800

    program for pt2

--- 17/day17.py
+++ 17/day17.py
@@ -51,7 +51,8 @@ if __name__ == '__main__':
             if (self.part2) and (int(self.output[-1]) != self.program[len(self.output)-1]):
                 self.ip = len(self.program) + 1
             else:
-                print(f"OUT{len(self.output)}: {self.output[-1]}, regA: {self.mem[7]} ({bin(self.mem[7])}) PROG: {self.program[len(self.output)-1]}")
+                if self.dbg:
+                    print(f"OUT{len(self.output)}: {self.output[-1]}, regA: {self.mem[7]} ({bin(self.mem[7])}) PROG: {self.program[len(self.output)-1]}")
                 self.ip += 2
         def bdv(self):
             #self.mem[5] = self.mem[4] // 2**self.mem[self.program[self.ip+1]]
@@ -62,7 +63,7 @@ if __name__ == '__main__':
             self.mem[6] = self.mem[4] >> self.mem[self.program[self.ip+1]]
             self.ip += 2
         def run(self):
-            if self.dbg:
+            if self.dbg and self.part2==False:
                 print(f"OP: {self.opcodes[self.program[self.ip]].__name__} LITERAL: {self.program[self.ip+1]} MEM: {self.mem[self.program[self.ip+1]]:08} (MEM%8: {self.mem[self.program[self.ip+1]]%8}) regA: {self.mem[4]} ({bin(self.mem[4])[-3:]}) regB: {self.mem[5]} regC: {self.mem[6]}")
             self.opcodes[self.program[self.ip]]()
 
@@ -113,28 +114,67 @@ if __name__ == '__main__':
 #    print(f"Test7: {test7CPU.output}")
 
     # find regA such that output = [2,4,1,2,7,5,1,3,4,4,5,5,0,3,3,0]
-    # 48 bit number (every ourput needs 3-9 bits)?
-    chain = [2,4,1,2,7,5,1,3,4,4,5,5,0,3,3,0]
-    part2 = []
-    for num in chain:
-        bin_num = [int(i) for i in bin(num)[2:]]
+    chain = [2,4,1,2,7,5,1,3,4,4,5,5,0,3,3,0] # length = 16
+    part2 = ''
+    done = False
 
-    #regA = 4037406697193472
-    #regA = 4037406697242100 # this got to entry 5
-    #regA = 4037408248217076 # this got to entry 9
-    #regA = 4118059680972276 # this got to entry 13
-    #regA = 4118609436786164 # this got to entry 15, increment by 2**33
-    #regA = 2087894516 # this zeroes out the upper bits
-
-    # too high: 37222274481652, I guess I'm going to try and count down
-    regA = 37222274481651
-    regA = 37222273957364 # this is correct
-
-    while (part2 != chain):
+    # MAX 48 bit number (every output needs 3 bits minimum)
+    regA = 0
+    increment = 0 # power of 2 to increment by (also number of already good bits)
+    while (done != True):
         part2CPU = CPU(regA, 0, 0, chain, dbg=options.dbg, part2=True)
         while (part2CPU.ip < len(part2CPU.program)):
             part2CPU.run()
-        #print(f"regA: {regA} {part2CPU.output}")
-        part2 = [int(i) for i in part2CPU.output]
-        regA -= 1
+        # 5 numbers =  3 good bits [2:0], increment by 2**3
+        # 6 numbers =  6 good bits [5:0], increnemt by 2**6
+        # 7 numbers =  9 good bits [9:0], increment by 2**9
+        # 8 numbers =  12 good bits [11:0], increment by 2**12
+        # 9 numbers =  15 good bits [14:0], increment by 2**15
+        # 10 numbers = 18 good bits [17:0], increment by 2**18
+        # 11 numbers = 21 good bits [20:0], increment by 2**21
+        # 12 numbers = 24 good bits [23:0], increment by 2**24
+        # 13 numbers = 27 good bits [26:0], increment by 2**27
+        # 14 numbers = 30 good bits [29:0], increment by 2**30
+        # 15 numbers = 33 good bits [32:0], increment by 2**33
+        # 16 numbers = 36 good bits [35:0], increment by 2**36
+        good_bits = 3*(len(part2CPU.output)-5)
+        if options.dbg:
+            print(f"{regA} num good bits {good_bits}, Part2: {part2} {len(part2)}")
+        if ( good_bits > len(part2) ):
+            part2 = bin(regA)[-good_bits:]
+            #regA = int(8**len(part2CPU.output)) + int(part2, base=2)
+            regA = 35184372088832 + int(part2, base=2)
+            increment = good_bits
+            if options.dbg:
+                print(f"LOCKED : {bin(regA)[-good_bits:]}, {part2}, {increment}, {regA}")
+            done = len(part2CPU.output) == len(chain)
+        regA += int( 2**increment )
     print(f"seed: {part2CPU.mem[7]} OUT: {','.join(part2CPU.output)}")
+
+## Trying a Map but I think I'm done for now
+#    bits = ['0' for i in range((3*(16+3)))]  # +3 because the MSB technically needs to be able to look ahead 
+#
+#    # dependencies for bit sequence (a2,a1,a0)
+#    # key : bits in number
+#    # val : bit to xor with
+#    seqmap = {0: (4,3,2),  # '000' -> (a4,a3,1)
+#              1: (5,4,3),  # '001' -> (a5,a4,a3)
+#              2: (2,1,0),  # '010' -> (0,0,0)
+#              3: (3,2,1),  # '011' -> (a3,1,1)
+#              4: (8,7,6),  # '100' -> (!a8,a7,!a6)
+#              5: (9,8,7),  # '101' -> (!a9,a8,a7)
+#              6: (6,5,4),  # '110' -> (!a6,!a5,!a4)
+#              7: (7,6,5) } # '111' -> (!a7,!a6,a5)
+#    for idx, num in enumerate(chain[::-1]):
+#        bit = 45 - idx
+#        print(idx, num)
+#        for key,rel_bit in seqmap.items():
+#            bin_key = format(key, '03b')
+#            bin_key_ints = [int(i) for i in bin_key]
+#            
+#            msb = bits[bin_key_ints[0]]^bits[rel_bit[0]]
+#            middle = bits[bin_key_ints[1]]^bits[rel_bit[1]]
+#            lsb = bits[bin_key_ints[2]]^bits[rel_bit[2]]
+#            print(msb, type(msb)
+#    print(bits)
+
