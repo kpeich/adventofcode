183421e9c7dfb703bab8392aad795af0612f0bfe
kpeich <13398635+kpeich@users.noreply.github.com>
2024-12-27 13:36:37 -0800

    finished boxing day

--- 15/day15.py
+++ 15/day15.py
@@ -1,4 +1,5 @@
 import optparse
+from copy import deepcopy
 
 def move(obj, loc, direction, grid, dbg=False):
     moves = { '^':(-1,0), 'v':(1,0), '<':(0,-1), '>':(0,1) }
@@ -9,8 +10,8 @@ def move(obj, loc, direction, grid, dbg=False):
         grid[row][col] = '.'
         grid[row+dr][col+dc] = obj
         return (row+dr,col+dc), grid
-    elif grid[row+dr][col+dc] == 'O':
-        queue = [(obj,loc),('O',(row+dr,col+dc))]
+    elif grid[row+dr][col+dc] in 'O[]':
+        queue = [(obj,loc),(grid[row+dr][col+dc],(row+dr,col+dc))]
         while queue:
             if dbg:
                 [print(''.join(i)) for i in grid]
@@ -28,8 +29,61 @@ def move(obj, loc, direction, grid, dbg=False):
             print("SOMETHING WENT WRONG?")
         return loc, grid
 
-def get_gps(grid):
-    return sum([sum([rdx*100+cdx for cdx,col in enumerate(row) if col == 'O']) for rdx,row in enumerate(grid)])
+def get_gps(grid, marker='O'):
+    return sum([sum([rdx*100+cdx for cdx,col in enumerate(row) if col == marker]) for rdx,row in enumerate(grid)])
+
+def doublewide(grid):
+    doublewide = []
+    for rdx,row in enumerate(grid):
+        doublewide.append([])
+        for col in row:
+            if col == '#':
+                doublewide[rdx] += ['#','#']
+            elif col == 'O':
+                doublewide[rdx] += ['[',']']
+            elif col == '.':
+                doublewide[rdx] += ['.','.']
+            elif col == '@':
+                doublewide[rdx] += ['@','.']
+    return doublewide
+
+def move2(obj, loc, direction, grid, dbg=False):
+    moves = { '^':(-1,0), 'v':(1,0) }
+    row,col = loc
+    dr,dc = moves[direction]
+    tgrid = deepcopy(grid)
+
+    if grid[row+dr][col+dc] == '.':
+        if dbg:
+            print(f"{obj} at {loc} free to move")
+        grid[row][col] = '.'
+        grid[row+dr][col+dc] = obj
+        return (row+dr,col+dc), grid
+    elif grid[row+dr][col+dc] in '[]':
+        box = { '[': 1, ']': -1}
+        dc = box[grid[row+dr][col+dc]] # if [ need to also check col+1, if ] need to check col-1
+
+        queue = [(obj,loc),(grid[row+dr][col],(row+dr,col)),(grid[row+dr][col+dc],(row+dr,col+dc))]
+        while queue:
+            if dbg:
+                [print(''.join(i)) for i in grid]
+            tobj, tloc = queue.pop()
+            if dbg:
+                print(f"TRY MOVE {obj} at {loc}")
+            moved, grid = move2(tobj, tloc, direction, grid)
+            if moved == tloc:
+                if dbg:
+                    print(f"{tobj} AT {tloc} DIDN'T MOVE")
+                return loc, tgrid
+        return moved, grid
+    
+    elif grid[row+dr][col+dc] == '#':
+        return loc, grid
+    else:
+        if dbg:
+            print(f"SOMETHING WENT WRONG? {grid[row+dr][col+dc]} {obj} {loc} {direction}")
+        return loc, grid
+
 
 if __name__ == '__main__':
 
@@ -40,7 +94,7 @@ if __name__ == '__main__':
     options,args = parser.parse_args()
 
     with open(options.filename) as f:
-        start = None
+        loc = None
         grid = []
         movement = ''
         for idx,line in enumerate(f):
@@ -50,10 +104,12 @@ if __name__ == '__main__':
                 movement = movement + line.strip()
             if '@' in line:
                 loc = (idx,line.index('@'))
+    grid2 = doublewide(grid)
+    loc2 = (loc[0],loc[1]*2)
 
     if options.dbg:
         [print(i) for i in grid]
-        print(start)
+        print(loc)
         print(movement)
 
     for direction in movement:
@@ -66,3 +122,23 @@ if __name__ == '__main__':
 
     part1 = get_gps(grid)
     print(f"Part1: {part1}")
+
+    if options.dbg:
+        [print(i) for i in grid2]
+        print(loc2)
+        print(movement)
+    for step,direction in enumerate(movement):
+        if options.dbg:
+            print(f"{step} MOVE {loc2}: {direction}")
+
+        if direction in '<>':
+            loc2, grid2 = move('@',loc2,direction,grid2, dbg=options.dbg)
+        elif direction in '^v':
+            loc2, grid2 = move2('@',loc2,direction,grid2, dbg=options.dbg)
+
+        if options.dbg:
+            [print(''.join(row)) for row in grid2]
+            print()
+            
+    part2 = get_gps(grid2,marker='[')
+    print(f"Part2: {part2}")
