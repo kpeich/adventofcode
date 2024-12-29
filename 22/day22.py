baf3cec298bd282cd13ae51b005e19949dc9415e
Kyle Eichenberger <13398635+kpeich@users.noreply.github.com>
2024-12-29 14:22:53 -0800

    part2 only needed a 4 banana window, not the whole history

--- 22/day22.py
+++ 22/day22.py
@@ -6,12 +6,9 @@ def mix(num, val):
 def prune(num, mod=16777216):
     return num % mod
 
-def gen_secret(num, prices=False):
-    print(num)
+def gen_secret(num):
     num = prune(mix(num,num << 6))
     num = prune(mix(num,num >> 5))
-    if prices:
-        return prune(mix(num,num << 11)), prune(mix(num,num << 11)) % 10
     return prune(mix(num,num << 11))
     
 
@@ -36,37 +33,26 @@ if __name__ == '__main__':
         print(nums)
 
     nums2k = []
+    profit = {}
     for num in lines:
+        price = {}
         time = 0
-        nums = {0 : num}
+        nums = [None,None,None,num]
+        diffs = [None,None,None,None]
         while time < options.time:
             time +=1
-            nums[time]= gen_secret(nums[time-1])
-        nums2k.append(nums[options.time])
+            nums.pop(0)
+            nums.append(gen_secret(nums[-1]))
 
-    #print(f"part1: {nums2k}")
+            diffs.pop(0)
+            diffs.append(nums[-1]%10 - nums[-2]%10)
+            if None not in diffs:
+                price[tuple(diffs)] = price.setdefault(tuple(diffs), nums[-1] % 10)
+        for key,val in price.items():
+            profit[key] = profit.setdefault(key, 0) + val
+        nums2k.append(nums[-1])
+
     print(f"part1: {sum(nums2k)}")
-
-#    if options.dbg:
-#        print(lines)
-#        time = 0
-#        nums = {0 : (123,123%10,None)}
-#        while time < options.time:
-#            time += 1
-#            num, price = gen_secret(nums[time-1][0], prices=True)
-#            nums[time] = (num, price, price-nums[time-1][1])
-#        [print(i) for i in nums.items()]
-
-    prices2k = {}
-    for idx,num in enumerate(lines):
-        time = 0
-        nums = {0 : (num,num%10,None)}
-        while time < options.time:
-            time +=1
-            num, price = gen_secret(nums[time-1][0], prices=True)
-            nums[time] = (num, price, price-nums[time-1][1])
-        prices2k[idx] = nums
-
-    [print(i) for i in prices2k.items()]
-    #print(f"part2: {prices2k}")
+    sales_key = max(profit,key=profit.get)
+    print(f"part2: {profit[sales_key]} bananas at {sales_key}")
 
