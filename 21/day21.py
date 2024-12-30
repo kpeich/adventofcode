75992c6b6ba1a27ad77de68faac1341d94fe3ceb
kpeich <13398635+kpeich@users.noreply.github.com>
2024-12-30 13:25:02 -0800

    the priority has been figured out : day21pt1 DONE

--- 21/day21.py
+++ 21/day21.py
@@ -10,7 +10,7 @@ class Robot():
         self.position = None
         self.pad = None
         self.seq = None
-        self.pressed = []
+        self.pressed = ''
 
     def get_adjacent(self, pos):
         row, col = pos
@@ -18,6 +18,9 @@ class Robot():
         return [(x,y) for x,y in adjacent if (x,y) in self.pad]
 
     def move(self, button):
+        '''
+        not functional, need to generalize the priority moves
+        '''
         #move_dict = { '<':(0,-1), '>':(0,1), '^':(1,0), 'v':(-1,0) }
         move_dict = { (0,-1):'<', (0,1):'>', (1,0):'^', (-1,0):'v' }
 
@@ -33,20 +36,20 @@ class Robot():
                     queue.extend([ (i, path + [ (i[0]-loc[0],i[1]-loc[1]) ]) for i in self.get_adjacent(loc) ])
                     visited.add(loc)
             else:
-                if (self.position[1] - loc[1] == 2) and ( ((0,0) not in self.pad and self.position[0] != 0) or ((0,0) in self.pad and self.position[0] == 1)):
-                    # move left first if we need to do it twice, but mind the gap (there is something more to the order here)
-                    self.pressed = self.pressed + [move_dict[move] for move in path][::-1] + ['A']
-                else:
-                    self.pressed = self.pressed + [move_dict[move] for move in path] + ['A']
+                moves = ''.join([move_dict[move] for move in path])
+                while self.pad[self.position] in moves:
+                    self.pressed.append(self.pad[self.position])
+                    moves.pop(moves.index(self.pad[self.position]))
+                self.pressed = self.pressed + moves + 'A'
                 self.position = loc
 #                print(f"FOUND {button} with {self.pressed} now at {self.position}")
                 return
 
     def print_movement(self):
-        print(''.join(self.pressed))
+        print(self.name, len(self.pressed), self.pressed)
 
     def reset(self):
-        self.pressed = []
+        self.pressed = ''
         self.alive = True
         self.seq = None
 
@@ -56,6 +59,19 @@ class Directional(Robot):
         self.position = (1,2)
         self.pad ={           (1,1):'^', (1,2):'A', 
                    (0,0):'<', (0,1):'v' ,(0,2):'>' }
+
+        self.best_moves = {'A': {'<':'v<<', '>':'v', '^':'<', 'v':'<v'}, 
+                           '^': {'A':'>', '<':'v<', '>':'v>', 'v':'v'},
+                           'v': {'A':'^>', '<':'<', '>':'>', '^':'^'},
+                           '<': {'A':'>>^', '>':'>>', '^':'>^', 'v':'>'}, 
+                           '>': {'A':'^', '<':'<<', '^':'<^', 'v':'<'}} 
+    def move(self, button):
+        if self.pad[self.position] == button:
+            self.pressed = self.pressed + 'A'
+        else:
+            self.pressed = self.pressed + self.best_moves[self.pad[self.position]][button] + 'A'
+        self.position =  [pos for pos,val in self.pad.items() if val == button][0]
+
     def reset(self):
         super().reset()
         self.position = (1,2)
@@ -69,10 +85,34 @@ class Numeric(Robot):
                     (2,0):'4', (2,1):'5', (2,2):'6',
                     (1,0):'1', (1,1):'2', (1,2):'3',
                                (0,1):'0', (0,2):'A'}
+
+        # not sure if this is entirely correct, but it passes my input/test
+        # make sure to prioritize the double move between < <-> A in the following steps
+            # basically if you have < it should be at the beginning or end of a sequence
+            # but also preserve double taps
+        self.best_moves = {'A': {'0':'<', '1':'^<<', '2':'<^', '3':'^', '4':'^^<<', '5':'^^<', '6':'^^', '7':'^^^<<', '8':'<^^^', '9':'^^^'}, 
+                           '0': {'A':'>', '1':'^<', '2':'^', '3':'>^', '4':'^^<', '5':'^<', '6':'>^', '7':'^^^<', '8':'^^^', '9':'>^^^'},
+                           '1': {'0':'>v', 'A':'>>v', '2':'>', '3':'>>', '4':'^', '5':'>^', '6':'>>^', '7':'^^', '8':'>^^', '9':'>>^^'},
+                           '2': {'0':'v', '1':'<', 'A':'v>', '3':'>', '4':'^<', '5':'^', '6':'>^', '7':'<^^', '8':'^^', '9':'>^^'},
+                           '3': {'0':'<v', '1':'<<', '2':'<', 'A':'v', '4':'<<^', '5':'<^', '6':'^', '7':'<<^^', '8':'<^^', '9':'^^'},
+                           '4': {'0':'>vv', '1':'v', '2':'>v', '3':'>>v', 'A':'>>vv', '5':'>', '6':'>>', '7':'^', '8':'>^', '9':'>>^'},
+                           '5': {'0':'vv', '1':'<v', '2':'v', '3':'>v', '4':'<', 'A':'vv>', '6':'>', '7':'<^', '8':'^', '9':'^>'},
+                           '6': {'0':'<vv', '1':'<<v', '2':'<v', '3':'v', '4':'<<', '5':'<', 'A':'vv', '7':'<<^', '8':'<^', '9':'^'},
+                           '7': {'0':'>vvv', '1':'vv', '2':'>vv', '3':'vv>>', '4':'v', '5':'>v', '6':'v>>', 'A':'>>vvv', '8':'>', '9':'>>'},
+                           '8': {'0':'vvv', '1':'<vv', '2':'vv', '3':'vv>', '4':'<v', '5':'v', '6':'>v', '7':'<', 'A':'vvv>', '9':'>'},
+                           '9': {'0':'vvv<', '1':'<<vv', '2':'<vv', '3':'vv', '4':'<<v', '5':'<v', '6':'v', '7':'<<', '8':'<', 'A':'vvv'}}
+    def move(self, button):
+        if self.pad[self.position] == button:
+            self.pressed = self.pressed + 'A'
+        else:
+            self.pressed = self.pressed + self.best_moves[self.pad[self.position]][button] + 'A'
+        self.position =  [pos for pos,val in self.pad.items() if val == button][0]
+
     def get_moves(self):
         for code in self.codes:
             for button in code:
                 self.move(button)
+
     def reset(self):
         super().reset()
         self.position = (0,2)
@@ -90,29 +130,20 @@ if __name__ == '__main__':
 
     rad = Directional('Brad')
     cold = Directional('Iceman')
-    space = Numeric('NoOxy', test)
-    #space = Numeric('NoOxy', data)
+    #space = Numeric('NoOxy', test)
+    space = Numeric('NoOxy', data)
 
+    robots = [space,cold,rad]
     complexities = {}
-    for code in space.codes:
-        space.reset()
+    for code in robots[0].codes:
+        robots[0].reset()
         for button in code:
-            space.move(button)
-            cold.reset()
-        for button in ''.join(space.pressed):
-            cold.move(button)
-            rad.reset()
-        for button in ''.join(cold.pressed):
-            rad.move(button)
-
-        rad.print_movement()
-        cold.print_movement()
-        space.print_movement()
-        print(code)
-        print(f"{len(rad.pressed)} * {int(code[:-1])} = {int(code[:-1]) * len(rad.pressed)}")
+            robots[0].move(button)
+        for idx,robot in enumerate(robots[1:]):
+            robot.reset()
+            for press in robots[idx].pressed:
+                robot.move(press)
+        print(f"{code}: {len(robots[-1].pressed)} * {int(code[:-1])} = {int(code[:-1]) * len(robots[-1].pressed)}")
         complexities[code] = int(code[:-1]) * len(rad.pressed)
-        #178770 too high
-    print(f"part1: {sum([num for code,num in complexities.items()])}")
+    print(f"part1: {sum([num for code,num in complexities.items()])}") #169390
 
-    part2 = 0
-    print(f"part2: {part2}")
