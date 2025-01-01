1e1fdd39c61e879a7b02d35160c0ca01ccf78a18
Kyle Eichenberger <13398635+kpeich@users.noreply.github.com>
2025-01-01 11:39:04 -0800

    md5 hashing

--- 2015/04/day04.py
+++ 2015/04/day04.py
@@ -1,4 +1,9 @@
 import optparse
+from hashlib import md5
+
+data = 'iwrupvqb'
+test1 = 'abcdef'
+test2 = 'pqrstuv'
 
 if __name__ == '__main__':
     parser = optparse.OptionParser()
@@ -7,14 +12,17 @@ if __name__ == '__main__':
 
     options,args = parser.parse_args()
 
-    with open(options.filename) as f:
-        data = [line.strip() for line in f]
+#    with open(options.filename) as f:
+#        data = [line.strip() for line in f]
 
     if options.dbg:
         pass
 
+    output = ''
     part1 = 0
-    print(f"part1: {part1}")
-
-    part2 = 0
-    print(f"part2: {part2}")
+    while output[:6] != '000000':
+        part1 += 1
+        string = f"{data}{part1}"
+        output = md5(string.encode()).hexdigest()
+        if output.startswith('00000'):
+            print(f"hash: {part1} : {output}")
