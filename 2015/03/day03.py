37496fe39d5d986084350ce7f58fb0b246ed19c0
Kyle Eichenberger <13398635+kpeich@users.noreply.github.com>
2025-01-01 10:45:30 -0800

    day03 done

--- 2015/03/day03.py
+++ 2015/03/day03.py
@@ -8,13 +8,25 @@ if __name__ == '__main__':
     options,args = parser.parse_args()
 
     with open(options.filename) as f:
-        data = [line.strip() for line in f]
+        data = [line.strip() for line in f][0]
 
     if options.dbg:
         pass
 
-    part1 = 0
-    print(f"part1: {part1}")
+    house = [0,0]
+    moves = {'<':(0,-1), '>':(0,1), '^':(1,0), 'v':(-1,0)}
+    presents = { (0,0):1 }
+    for move in data:
+        house = [sum(i) for i in zip(house,moves[move])]
+        presents[tuple(house)] = presents.setdefault(tuple(house), 1) + 1
 
-    part2 = 0
-    print(f"part2: {part2}")
+    print(f"part1: {len(presents)}")
+
+    houses = [[0,0],[0,0]]
+    presents = {(0,0):2}
+    for idx in range(0,len(data),2):
+        houses[0] = [sum(i) for i in zip(houses[0],moves[data[idx]])]
+        houses[1] = [sum(i) for i in zip(houses[1],moves[data[idx+1]])]
+        presents[tuple(houses[0])] = presents.setdefault(tuple(houses[0]), 1) + 1
+        presents[tuple(houses[1])] = presents.setdefault(tuple(houses[1]), 1) + 1
+    print(f"part2: {len(presents)}")
