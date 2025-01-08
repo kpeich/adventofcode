12fdc54634cb52f8a502497f7460c0ce783bce79
kpeich <13398635+kpeich@users.noreply.github.com>
2025-01-08 15:22:18 -0800

    day05 and day06

--- 2015/05/day05.py
+++ 2015/05/day05.py
@@ -1,5 +1,40 @@
 import optparse
 
+def check_string(string):
+    invalid = ('ab','cd','pq','xy')
+    vowels = 'aeiou'
+    num_vowels = 0
+    double = False
+
+    for idx,ch in enumerate(string):
+        if ch in vowels:
+            num_vowels += 1
+        try:
+            if string[idx+1] == ch:
+                double = True
+            if ch+string[idx+1] in invalid:
+                return False
+        except:
+            continue
+
+    return num_vowels > 2 and double 
+
+def check_string2(string):
+    has_pair = False
+    has_gap = False
+
+    for idx,ch in enumerate(string):
+        try:
+            if string[idx+2] == ch:
+                has_gap = True
+        except IndexError:
+            continue
+        if string[idx-1]+ch in string[idx+1:] and idx != 0:
+            has_pair = True
+
+    return has_pair and has_gap
+
+
 if __name__ == '__main__':
     parser = optparse.OptionParser()
     parser.add_option("-f", "--file", dest="filename", help="input filename")
@@ -13,8 +48,8 @@ if __name__ == '__main__':
     if options.dbg:
         pass
 
-    part1 = 0
-    print(f"part1: {part1}")
+    part1 = [check_string(string) for string in data]
+    print(f"part1: {sum(part1)}")
 
-    part2 = 0
-    print(f"part2: {part2}")
+    part2 = [check_string2(string) for string in data]
+    print(f"part2: {sum(part2)}")
--- 2015/06/day06.py
+++ 2015/06/day06.py
@@ -1,4 +1,19 @@
 import optparse
+import re
+
+def on(x,y,grid):
+    return True
+def off(x,y,grid):
+    return False
+def flip(x,y,grid):
+    return not grid[x][y]
+
+def inc(x,y,grid):
+    return 1
+def dec(x,y,grid):
+    return -1
+def incc(x,y,grid):
+    return 2
 
 if __name__ == '__main__':
     parser = optparse.OptionParser()
@@ -7,14 +22,36 @@ if __name__ == '__main__':
 
     options,args = parser.parse_args()
 
+    data = []
     with open(options.filename) as f:
-        data = [line.strip() for line in f]
+        for line in f:
+            fn = re.match(r'(.*) (\d+),(\d+).* (\d+),(\d+).*$', line)
+            inst = fn.group(1)
+            grid = tuple( int(fn.group(i)) for i in range(2,6) )
+            data.append((inst,grid))
 
     if options.dbg:
         pass
 
-    part1 = 0
-    print(f"part1: {part1}")
+    # part1
+    lights = [[False for j in range(1000)]for i in range(1000)]
+    code_dict = {'turn on': on, 'turn off': off, 'toggle': flip}
+    for instr,coords in data:
+        if options.dbg:
+            print(instr,coords)
+        for i in range(coords[0],coords[2]+1):
+            for j in range(coords[1], coords[3]+1):
+                lights[i][j] = code_dict[instr](i,j,lights)
+    print(f"part1: {sum([sum(row) for row in lights])}")
 
-    part2 = 0
-    print(f"part2: {part2}")
+    # part2
+    lights2 = [[0 for j in range(1000)]for i in range(1000)]
+    code_dict2 = {'turn on': inc, 'turn off': dec, 'toggle': incc}
+    for instr,coords in data:
+        if options.dbg:
+            print(instr,coords)
+        for i in range(coords[0],coords[2]+1):
+            for j in range(coords[1], coords[3]+1):
+                lights2[i][j] += code_dict2[instr](i,j,lights2)
+                lights2[i][j] = 0 if lights2[i][j] < 0 else lights2[i][j]
+    print(f"part2: {sum([sum(row) for row in lights2])}")
