1dc533026bfe30426cb5ca77121fd8483c1f0b39
kpeich <13398635+kpeich@users.noreply.github.com>
2025-01-08 22:30:13 -0800

    encode/decode strings

--- 2015/08/day08.py
+++ 2015/08/day08.py
@@ -1,4 +1,5 @@
 import optparse
+import re
 
 if __name__ == '__main__':
     parser = optparse.OptionParser()
@@ -14,7 +15,27 @@ if __name__ == '__main__':
         pass
 
     part1 = 0
+    for line in data:
+        num_literal = len(line)
+        print(line)
+        line = re.sub(r'\\x[0-9A-Fa-f][0-9A-Fa-f]','x',line) # every ascii is just an x
+        line = line.replace('\\"', '"')
+        line = line.replace('\\\\', '\\')
+        print(line)
+        num_mem = len(line[1:-1])
+
+        part1 += num_literal - num_mem
     print(f"part1: {part1}")
 
     part2 = 0
+    for line in data:
+        num_literal = len(line)
+        print(line)
+        line = line.replace('\\', '\\\\')
+        line = line.replace('"', '\\"')
+        print(line)
+        num_mem = len(line) + 2 # add 2 for the outermost double quotes
+        print(num_literal, num_mem)
+
+        part2 += num_mem - num_literal 
     print(f"part2: {part2}")
